70ebf3edb75785fd927ccea8ae0a5dbc118f7433
nayyyhaa <nehaguptang.ng@gmail.com>
2021-10-21 23:57:40 +0530

    refactor: site title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Neha Gupta";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Blog Site";
 
 export default function Layout({ children, home }) {
   return (
